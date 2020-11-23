f2225c1f05247e68d7b9c840d1485fd3bbfbe886
Vikram Chatterjee <strycora@gmail.com>
2020-11-22 19:12:14 -0700

    commit tree b403c3b651c7f7eee60badb8f49f73dad0d26e73

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,26 @@
-def starts_with_a_vowel?(word)
+require "pry"
 
+def starts_with_a_vowel?(word)
+    #binding.pry
+   !!word.match(/\b[aeiou]\w*/i)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    #binding.pry
+    text.scan(/un\w{1,10}ing/)
 end
 
 def words_five_letters_long(text)
-
+    #binding.pry
+    text.scan /\b\w{5}\b/
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    #binding.pry
+    !!text.match(/^[A-Z].*\W$/)
 end
 
 def valid_phone_number?(phone)
-
+    #binding.pry
+    !!phone.match(/\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\z/)
 end
